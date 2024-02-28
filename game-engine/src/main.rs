f95c3e0480b7ea28d3d00706d382284cfb16d792
xpakx <55586470+xpakx@users.noreply.github.com>
2024-02-28 14:51:48 +0100

    feat(engine): implement generating hit for random engine

--- game-engine/src/main.rs
+++ game-engine/src/main.rs
@@ -1,7 +1,24 @@
+use rand::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut engine = RandomEngine::new();
+    println!("{}", engine.get_name());
+    let shot = engine.get_shot(
+        &BoardState 
+        { 
+            board: vec![
+                vec![Field::Empty, Field::Hit, Field::Empty, Field::Drown], 
+                vec![Field::Empty, Field::Empty, Field::Empty, Field::Miss], 
+                vec![Field::Empty, Field::Empty, Field::Empty, Field::Empty], 
+                vec![Field::Empty, Field::Empty, Field::Empty, Field::Empty], 
+            ], 
+            remaining_ships: vec![1, 1, 2]
+        }
+        );
+    println!("{}, {}", shot.x, shot.y);
 }
 
+#[derive(Clone, Copy)]
 struct Pos {
     x: i32,
     y: i32,
@@ -18,6 +35,7 @@ struct BoardDefinition {
     height: i32,
 }
 
+#[derive(PartialEq)]
 enum Field {
     Empty,
     Hit,
@@ -33,10 +51,22 @@ struct BoardState {
 trait Engine {
     fn get_name(&self) -> String;
     fn place_ships(&self, board: &BoardDefinition, ships: Vec<i32>) -> Vec<Ship>;
-    fn get_shot(&self, board: &BoardState) -> Pos;
+    fn get_shot(&mut self, board: &BoardState) -> Pos;
 }
 
-struct RandomEngine {}
+
+
+struct RandomEngine {
+    rng: ThreadRng,
+}
+
+impl RandomEngine {
+    fn new() -> RandomEngine {
+        RandomEngine {
+            rng: thread_rng(),
+        }
+    }
+}
 
 impl Engine for RandomEngine {
     fn get_name(&self) -> String {
@@ -47,7 +77,21 @@ impl Engine for RandomEngine {
         vec![]
     }
 
-    fn get_shot(&self, _board: &BoardState) -> Pos {
-        Pos {x: 0, y: 0}
+    fn get_shot(&mut self, board: &BoardState) -> Pos {
+        let mut empty_positions = Vec::new();
+
+        for (x, row) in board.board.iter().enumerate() {
+            for (y, field) in row.iter().enumerate() {
+                if field == &Field::Empty {
+                    empty_positions.push(Pos {x: x as i32, y: y as i32});
+                }
+            }
+        }
+
+        if empty_positions.is_empty() {
+            return Pos { x: -1, y: -1 };
+        }
+        let index = self.rng.gen_range(0..empty_positions.len());
+        empty_positions[index]
     }
 }
