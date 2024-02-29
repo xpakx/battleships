bbcd70d2adef9870dabe48b9dc86bccd52c64f51
xpakx <55586470+xpakx@users.noreply.github.com>
2024-02-29 14:01:45 +0100

    feat(engine): test ship placement in games with non-adjacent ships

--- game-engine/src/validator.rs
+++ game-engine/src/validator.rs
@@ -1,4 +1,4 @@
-use std::{usize, cmp::min};
+use std::{usize, cmp::{min, max}};
 
 use crate::{BoardDefinition, Ship};
 
@@ -20,10 +20,28 @@ pub fn check_ship_placement(board: &BoardDefinition, ships: &Vec<Ship>) -> bool 
             Orientation::Vertical => (ship.tail.x - ship.head.x + 1).abs() as usize,
             Orientation::Horizontal => (ship.tail.y - ship.head.y + 1).abs() as usize,
         };
+
         let start = match orientation {
             Orientation::Vertical => min(ship.head.x, ship.tail.x) as usize,
             Orientation::Horizontal => min(ship.head.y, ship.tail.y) as usize,
         };
+
+        if !board.adjacent_ships_allowed && start > 0 {
+            let x = match orientation {
+                Orientation::Vertical => start - 1,
+                Orientation::Horizontal => ship.head.x as usize,
+            };
+            let y = match orientation {
+                Orientation::Vertical => ship.head.y as usize,
+                Orientation::Horizontal => start - 1,
+            };
+            ships_placed[x][y] = true;
+            let (x_1, y_1) = get_prev_coord(&orientation, x, y);
+            ships_placed[x_1][y_1] = true;
+            let (x_2, y_2) = get_next_coord(board, &orientation, x, y);
+            ships_placed[x_2][y_2] = true;
+        }
+
 
         for i in 0..len {
             let x = match orientation {
@@ -38,12 +56,77 @@ pub fn check_ship_placement(board: &BoardDefinition, ships: &Vec<Ship>) -> bool 
                 return false
             }
             ships_placed[x][y] = true;
+
+            if !board.adjacent_ships_allowed {
+                let (x_1, y_1) = get_prev_coord(&orientation, x, y);
+                ships_placed[x_1][y_1] = true;
+                let (x_2, y_2) = get_next_coord(board, &orientation, x, y);
+                ships_placed[x_2][y_2] = true;
+            }
+        }
+
+
+        if !board.adjacent_ships_allowed {
+            let test = match orientation {
+                Orientation::Vertical => start + len < (board.height) as usize,
+                Orientation::Horizontal => start + len < (board.width) as usize,
+            };
+            if test {
+                let x = match orientation {
+                    Orientation::Vertical => start + len,
+                    Orientation::Horizontal => ship.head.x as usize,
+                };
+                let y = match orientation {
+                    Orientation::Vertical => ship.head.y as usize,
+                    Orientation::Horizontal => start + len,
+                };
+                ships_placed[x][y] = true;
+                let (x_1, y_1) = get_prev_coord(&orientation, x, y);
+                ships_placed[x_1][y_1] = true;
+                let (x_2, y_2) = get_next_coord(board, &orientation, x, y);
+                ships_placed[x_2][y_2] = true;
+            }
         }
     }
 
     true
 }
 
+fn get_prev_coord(orientation: &Orientation, x: usize, y: usize) -> (usize, usize) {
+    let x_1 = match orientation {
+        Orientation::Vertical => x,
+        Orientation::Horizontal => {
+            if x > 0 {
+                x-1 
+            } else { 
+                0
+            }
+        }
+    };
+    let y_1 = match orientation {
+        Orientation::Vertical => {
+            if y > 0 {
+                y-1
+            } else {
+                0
+            }
+        },
+        Orientation::Horizontal => y,
+    };
+    (x_1, y_1)
+}
+
+fn get_next_coord(board: &BoardDefinition, orientation: &Orientation, x: usize, y: usize) -> (usize, usize) {
+    let x_2 = match orientation {
+        Orientation::Vertical => x,
+        Orientation::Horizontal => min(board.width as usize - 1, x+1),
+    };
+    let y_2 = match orientation {
+        Orientation::Vertical => min(board.height as usize - 1, y+1),
+        Orientation::Horizontal => y,
+    };
+    (x_2, y_2)
+}
 
 #[cfg(test)]
 mod tests {
