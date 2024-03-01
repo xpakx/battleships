cea6ff7791e9903d5c7f5e054f9f7d25b018ea56
xpakx <55586470+xpakx@users.noreply.github.com>
2024-03-01 12:47:47 +0100

    chore(game): init websocket service

---
+++ game/src/test/java/io/github/xpakx/battleships/BattleshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xpakx.battleships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
