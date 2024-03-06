efd9e13196b3b02122738a48124c3db1aee324bb
xpakx <55586470+xpakx@users.noreply.github.com>
2024-03-06 12:29:31 +0100

    feat(engine): process events with placement validation

--- game-engine/src/rabbit/placement_client.rs
+++ game-engine/src/rabbit/placement_client.rs
@@ -1,7 +1,9 @@
 use lapin::{Channel, options::BasicAckOptions, message::DeliveryResult, Consumer};
 
 use serde::{Serialize, Deserialize};
-use crate::rabbit::DESTINATION_EXCHANGE;
+use crate::{rabbit::DESTINATION_EXCHANGE, data::{Ship, Pos, Orientation, BoardDefinition}, validator::{check_ship_placement, check_ships_are_on_board, check_all_ships_are_placed}};
+
+use super::ai_client::ShipMsg;
 
 pub fn set_delegate(consumer: Consumer, channel: Channel) {
     consumer.set_delegate({
@@ -58,18 +60,18 @@ pub fn set_delegate(consumer: Consumer, channel: Channel) {
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct PlacementMessage {
-    game_state: String,
     game_id: i32,
-    column: Option<i32>,
-    row: Option<i32>,
-    ai: bool,
-    current_symbol: String,
+    first_user: bool,
+    ships: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct EngineEvent {
     game_id: i32,
+    first_user: bool,
+    ships: String,
+    legal: bool,
     malformed: Option<bool>,
 }
 
@@ -77,11 +79,39 @@ impl Default for EngineEvent {
     fn default() -> EngineEvent {
         EngineEvent { 
             game_id: -1,
+            first_user: false,
+            ships: String::from(""),
+            legal: false,
             malformed: None,
         } 
     } 
 }
 
 fn process_placement_event(game_msg: &PlacementMessage) -> EngineEvent {
-    Default::default()
+    let ships: Vec<ShipMsg> = match serde_json::from_str(&game_msg.ships) {
+        Ok(ships) => ships,
+        Err(_) => {
+            return Default::default();
+        }
+    };
+    let ships: Vec<Ship> = ships.iter().map(|ship| Ship {
+        head: Pos{x: ship.head_x, y: ship.head_y},
+        size: ship.size,
+        orientation: match ship.orientation {
+            super::ai_client::Orientation::Vertical => Orientation::Vertical,
+            super::ai_client::Orientation::Horizontal => Orientation::Horizontal,
+        },
+    }).collect();
+    let board_definition = BoardDefinition { width: 10, height: 10, adjacent_ships_allowed: false };
+    let sizes = vec![1, 1, 1, 1, 2, 2, 2, 3, 3, 4];
+    let correct = check_ship_placement(&board_definition, &ships);
+    let on_board = check_ships_are_on_board(&board_definition, &ships);
+    let all = check_all_ships_are_placed(&ships, sizes);
+    EngineEvent { 
+        game_id: game_msg.game_id,
+        first_user: game_msg.first_user,
+        ships: game_msg.ships.clone(),
+        legal: correct && on_board && all,
+        malformed: None,
+    }
 }
