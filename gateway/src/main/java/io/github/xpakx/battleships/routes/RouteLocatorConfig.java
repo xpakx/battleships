afa1e165498edc81a15ba0cbd9dc63210421780c
xpakx <55586470+xpakx@users.noreply.github.com>
2024-03-10 12:42:05 +0100

    chore(gateway): add filter for deduplicating headers to RouteLocators

--- gateway/src/main/java/io/github/xpakx/battleships/routes/RouteLocatorConfig.java
+++ gateway/src/main/java/io/github/xpakx/battleships/routes/RouteLocatorConfig.java
@@ -1,5 +1,7 @@
 package io.github.xpakx.battleships.routes;
 
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.DedupeResponseHeaderGatewayFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,15 +11,30 @@ import org.springframework.http.HttpHeaders;
 @Configuration
 public class RouteLocatorConfig {
     @Bean
-    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+    public RouteLocator myRoutes(RouteLocatorBuilder builder, GatewayFilter dedupeResponseHeaderFilter) {
         return builder.routes()
                 .route("main", r -> r
                         .path("/authenticate", "/register", "/game/**")
-                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .filters(f -> f.filter(dedupeResponseHeaderFilter))
                         .uri("http://localhost:8080"))
                 .route("game", r -> r
                         .path("/app/**", "/topic/**", "/play/**")
+                        .filters(f -> f.filter(dedupeResponseHeaderFilter))
                         .uri("http://localhost:8081"))
                 .build();
+    }
+
+    @Bean
+    public GatewayFilter dedupeResponseHeaderFilter() {
+        return new DedupeResponseHeaderGatewayFilterFactory()
+                .apply(config -> {
+                    config
+                            .setName(
+                                    HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS + ", " + HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN
+                            );
+                    config
+                            .setStrategy(DedupeResponseHeaderGatewayFilterFactory.Strategy.RETAIN_UNIQUE);
+                        }
+                );
     }
 }
