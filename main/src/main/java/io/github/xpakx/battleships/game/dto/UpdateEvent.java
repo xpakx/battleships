3600b799855a527943b9ad8367c3e612d9302636
xpakx <55586470+xpakx@users.noreply.github.com>
2024-03-15 12:52:05 +0100

    fix(main): fix serialization of UpdateEvent's timestamp

--- main/src/main/java/io/github/xpakx/battleships/game/dto/UpdateEvent.java
+++ main/src/main/java/io/github/xpakx/battleships/game/dto/UpdateEvent.java
@@ -1,5 +1,10 @@
 package io.github.xpakx.battleships.game.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,6 +27,9 @@ public class UpdateEvent {
 
     private boolean userTurn;
 
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 
     private Integer lastMoveX;
