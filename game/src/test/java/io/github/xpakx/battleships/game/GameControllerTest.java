2950410c79c7f44ba34443cef8569066360a7f0a
xpakx <55586470+xpakx@users.noreply.github.com>
2024-03-15 13:55:50 +0100

    test(game): test chat

--- game/src/test/java/io/github/xpakx/battleships/game/GameControllerTest.java
+++ game/src/test/java/io/github/xpakx/battleships/game/GameControllerTest.java
@@ -58,6 +58,8 @@ class GameControllerTest {
 
     WebSocketStompClient stompClient;
 
+    private CompletableFuture<ChatMessage> completableMessage;
+
     @Value("${jwt.secret}")
     String secret;
 
@@ -150,6 +152,7 @@ class GameControllerTest {
                 new StandardWebSocketClient()
         );
         stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+        completableMessage = new CompletableFuture<>();
         if (!rabbitSetupIsDone) {
             config();
         }
@@ -173,5 +176,115 @@ class GameControllerTest {
                 .untilAsserted(() -> {
                     assertThat(session.isConnected(), equalTo(true));
                 });
+    }
+
+    @Test
+    void shouldSubscribeChat() throws Exception {
+        StompSession session = stompClient
+                .connectAsync(baseUrl + "/play/websocket", new StompSessionHandlerAdapter() {})
+                .get(1, SECONDS);
+        await()
+                .atMost(1, SECONDS)
+                .until(session::isConnected);
+        CountDownLatch latch = new CountDownLatch(1);
+        session.subscribe("/topic/chat/1", new ChatFrameHandler(latch));
+        var msg = new ChatMessage();
+        msg.setMessage("Message");
+        msg.setPlayer("Guest");
+        Thread.sleep(100); // TODO: eliminate sleep
+        simpMessagingTemplate.convertAndSend("/topic/chat/1",  msg);
+        await()
+                .atMost(1, SECONDS)
+                .untilAsserted(() -> assertEquals(0, latch.getCount()));
+        ChatMessage chatMessage = completableMessage.get(1, SECONDS);
+        assertThat(chatMessage, notNullValue());
+        assertThat(chatMessage.getMessage(), equalTo("Message"));
+        assertThat(chatMessage.getPlayer(), equalTo("Guest"));
+    }
+
+    @Test
+    void shouldSendChatMessageByGuestUser() throws Exception {
+        StompSession session = stompClient
+                .connectAsync(baseUrl + "/play/websocket", new StompSessionHandlerAdapter() {})
+                .get(1, SECONDS);
+        await()
+                .atMost(1, SECONDS)
+                .until(session::isConnected);
+        CountDownLatch latch = new CountDownLatch(1);
+        session.subscribe("/topic/chat/1", new ChatFrameHandler(latch));
+        var msg = new ChatRequest();
+        msg.setMessage("Message");
+        session.send("/app/chat/1", msg);
+        await()
+                .atMost(1, SECONDS)
+                .untilAsserted(() -> assertEquals(0, latch.getCount()));
+        ChatMessage chatMessage = completableMessage.get(1, SECONDS);
+        assertThat(chatMessage, notNullValue());
+        assertThat(chatMessage.getMessage(), equalTo("Message"));
+        assertThat(chatMessage.getPlayer(), equalTo("guest"));
+    }
+
+    @Test
+    void shouldSendChatMessage() throws Exception {
+        StompHeaders stompHeaders = new StompHeaders();
+        stompHeaders.add("Token", generateToken("test_user"));
+        StompSession session = stompClient
+                .connectAsync(
+                        baseUrl + "/play/websocket" ,
+                        new WebSocketHttpHeaders(),
+                        stompHeaders,
+                        new StompSessionHandlerAdapter() {}
+                )
+                .get(1, SECONDS);
+        await()
+                .atMost(1, SECONDS)
+                .until(session::isConnected);
+        CountDownLatch latch = new CountDownLatch(1);
+        session.subscribe("/topic/chat/1", new ChatFrameHandler(latch));
+        var msg = new ChatRequest();
+        msg.setMessage("Message");
+        session.send("/app/chat/1", msg);
+        await()
+                .atMost(1, SECONDS)
+                .untilAsserted(() -> assertEquals(0, latch.getCount()));
+        ChatMessage chatMessage = completableMessage.get(1, SECONDS);
+        assertThat(chatMessage, notNullValue());
+        assertThat(chatMessage.getMessage(), equalTo("Message"));
+        assertThat(chatMessage.getPlayer(), equalTo("test_user"));
+    }
+
+    private class ChatFrameHandler implements StompFrameHandler {
+        private final CountDownLatch latch;
+
+        public ChatFrameHandler(CountDownLatch latch) {
+            this.latch = latch;
+        }
+        @Override
+        public Type getPayloadType(StompHeaders headers) {
+            return ChatMessage.class;
+        }
+
+        @Override
+        public void handleFrame(StompHeaders headers, Object payload) {
+            completableMessage.complete((ChatMessage) payload);
+            latch.countDown();
+        }
+
+    }
+
+
+    public String generateToken(String username) {
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put(
+                "roles",
+                List.of()
+        );
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 60 * 1000))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
     }
 }
