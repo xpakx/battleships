a51926699e0f361e863e41885169cac24ea8b181
xpakx <55586470+xpakx@users.noreply.github.com>
2024-03-19 13:24:16 +0100

    fix(main): fix saving ships to db

--- main/src/main/java/io/github/xpakx/battleships/game/UpdateEventHandler.java
+++ main/src/main/java/io/github/xpakx/battleships/game/UpdateEventHandler.java
@@ -1,7 +1,10 @@
 package io.github.xpakx.battleships.game;
 
+import io.github.xpakx.battleships.clients.GameEventHandler;
 import io.github.xpakx.battleships.game.dto.UpdateEvent;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
@@ -13,8 +16,11 @@ public class UpdateEventHandler {
     private final GameRepository repository;
     private final MoveService moveService;
 
+    Logger logger = LoggerFactory.getLogger(GameEventHandler.class);
+
     @RabbitListener(queues = "${amqp.queue.updates}")
     void handleGame(final UpdateEvent event) {
+        logger.debug("Got update event for game {}", event.getGameId());
         try {
             var game = repository.findById(event.getGameId());
             game.ifPresent((g) -> {
---
+++ main/src/main/resources/db/migration/V7__fix_ships_length.sql
@@ -0,0 +1,2 @@
+ALTER TABLE game ALTER COLUMN user_ships TYPE varchar(500);
+ALTER TABLE game ALTER COLUMN opponent_ships TYPE varchar(500);
