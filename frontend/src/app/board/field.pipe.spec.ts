6fe3d81e2bce1eadbc5829061dcdf3815f7e194b
xpakx <55586470+xpakx@users.noreply.github.com>
2024-03-20 15:01:55 +0100

    test(frontend): test pipe for board's field

--- frontend/src/app/board/field.pipe.spec.ts
+++ frontend/src/app/board/field.pipe.spec.ts
@@ -1,8 +1,29 @@
 import { FieldPipe } from './field.pipe';
 
 describe('FieldPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FieldPipe();
-    expect(pipe).toBeTruthy();
+  let pipe: FieldPipe;
+
+  beforeEach(() => {
+    pipe = new FieldPipe();
+  });
+
+  it('transforms "Sunk" to "✘"', () => {
+    const transformedValue = pipe.transform('Sunk');
+    expect(transformedValue).toEqual('✘');
+  });
+
+  it('transforms "Miss" to "o"', () => {
+    const transformedValue = pipe.transform('Miss');
+    expect(transformedValue).toEqual('o');
+  });
+
+  it('transforms "Hit" to "."', () => {
+    const transformedValue = pipe.transform('Hit');
+    expect(transformedValue).toEqual('.');
+  });
+
+  it('transforms unknown value to empty string', () => {
+    const transformedValue = pipe.transform('test');
+    expect(transformedValue).toEqual('');
   });
 });
