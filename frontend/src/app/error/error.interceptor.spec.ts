53ebe41c6bb61e6966706731c9afa4809399c425
xpakx <55586470+xpakx@users.noreply.github.com>
2024-03-20 14:00:30 +0100

    test(frontend): test error interceptor

--- frontend/src/app/error/error.interceptor.spec.ts
+++ frontend/src/app/error/error.interceptor.spec.ts
@@ -1,16 +1,67 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ErrorInterceptor } from './error.interceptor';
+import { HTTP_INTERCEPTORS, HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ErrorInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      ErrorInterceptor
-      ]
-  }));
+  let http: HttpClient;
+  let httpMock: HttpTestingController;
 
-  it('should be created', () => {
-    const interceptor: ErrorInterceptor = TestBed.inject(ErrorInterceptor);
-    expect(interceptor).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+      ]
+    });
+    http = TestBed.inject(HttpClient);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should remove token and username from localStorage on 401 error', () => {
+    const mockErrorResponse = new HttpErrorResponse({
+      status: 401,
+      statusText: 'Unauthorized'
+    });
+
+    spyOn(localStorage, 'removeItem');
+
+    http.get('/api/some-endpoint').subscribe({
+      next: () => { },
+      error: (error: HttpErrorResponse) => {
+        expect(error.status).toEqual(401);
+        expect(localStorage.removeItem).toHaveBeenCalledWith('token');
+        expect(localStorage.removeItem).toHaveBeenCalledWith('username');
+      }
+    }
+    );
+
+    const req = httpMock.expectOne('/api/some-endpoint');
+    req.flush("", mockErrorResponse);
+  });
+
+  it('should not modify localStorage on other errors', () => {
+    const mockErrorResponse = new HttpErrorResponse({
+      status: 404,
+      statusText: 'Not Found'
+    });
+
+    spyOn(localStorage, 'removeItem');
+
+    http.get('/api/some-endpoint').subscribe(
+      () => { },
+      (error: HttpErrorResponse) => {
+        expect(error.status).toEqual(404);
+        expect(localStorage.removeItem).not.toHaveBeenCalled();
+      }
+    );
+
+    const req = httpMock.expectOne('/api/some-endpoint');
+    req.flush("", mockErrorResponse);
   });
 });
