7a053a2f72fd9978d6fec22408269f95892fb014
xpakx <55586470+xpakx@users.noreply.github.com>
2024-03-20 12:57:04 +0100

    test(frontend): test game list component in requests mode

--- frontend/src/app/main/game-list/game-list.component.spec.ts
+++ frontend/src/app/main/game-list/game-list.component.spec.ts
@@ -3,6 +3,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GameListComponent } from './game-list.component';
 import { of } from 'rxjs';
 import { GameManagementService } from '../game-management.service';
+import { Component } from '@angular/core';
+import { Game } from '../dto/game';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('GameListComponent', () => {
@@ -60,4 +62,65 @@ describe('GameListComponent', () => {
     expect(component.openGame.emit).toHaveBeenCalledWith(gameId);
   });
 
+});
+
+@Component({
+  selector: 'app-test-host-component',
+  template: '<app-game-list [games]="games" [active]="active" [requests]="requests" (openGame)="onOpenGame($event)"></app-game-list>'
+})
+class TestRequestsHostComponent {
+  games: Game[] = [
+    { id: 1, username1: 'User1', username2: 'User2', currentState: [], lastMoveRow: 0, lastMoveColumn: 0, type: '', finished: false, won: false, lost: false, drawn: false, userStarts: true, currentSymbol: '', myShips: [] }
+  ];
+  active = false;
+  requests = true;
+  onOpenGame(gameId: number) {}
+}
+
+describe('GameListComponent (requests)', () => {
+  let testHostComponent: TestRequestsHostComponent;
+  let fixture: ComponentFixture<TestRequestsHostComponent>;
+  let gameService: jasmine.SpyObj<GameManagementService>;
+
+  beforeEach(async () => {
+    const gameServiceSpy = jasmine.createSpyObj('GameManagementService', ['acceptRequest', 'rejectRequest']);
+    await TestBed.configureTestingModule({
+      declarations: [GameListComponent, TestRequestsHostComponent],
+      imports: [HttpClientTestingModule],
+      providers: [{ provide: GameManagementService, useValue: gameServiceSpy }]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestRequestsHostComponent);
+    testHostComponent = fixture.componentInstance;
+    gameService = TestBed.inject(GameManagementService) as jasmine.SpyObj<GameManagementService>;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(testHostComponent).toBeTruthy();
+  });
+
+  it('should render buttons based on inputs', () => {
+    const buttons = fixture.nativeElement.querySelectorAll('button');
+    expect(buttons.length).toEqual(2);
+  });
+
+  it('should call acceptRequest when accept button is clicked', () => {
+    const gameId = 1;
+    const acceptButton = fixture.nativeElement.querySelector('button:nth-child(2)');
+    acceptButton.click();
+
+    expect(gameService.acceptRequest).toHaveBeenCalledWith(gameId, { accepted: true });
+  });
+
+  it('should call rejectRequest when reject button is clicked', () => {
+    const gameId = 1;
+    const rejectButton = fixture.nativeElement.querySelector('button:nth-child(3)');
+    rejectButton.click();
+
+    expect(gameService.acceptRequest).toHaveBeenCalledWith(gameId, { accepted: false });
+  });
 });
